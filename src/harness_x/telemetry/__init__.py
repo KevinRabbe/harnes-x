9e7533bd6b900846f7ed2d46d444627140bbcdb4
KevinRabbe <rabbekevin@icloud.com>
2026-08-18 21:50:52 +0200

    Export grounded telemetry APIs

--- src/harness_x/telemetry/__init__.py
+++ src/harness_x/telemetry/__init__.py
@@ -1,13 +1,21 @@
-"""Trace storage, replay, and later system telemetry."""
+"""Trace storage, replay, and grounded system telemetry."""
 
+from .metrics import JsonlMetricsStore, MetricsSample, RuntimeMetrics, derive_runtime_metrics
 from .replay import ReplayState, TraceReplayer
+from .self_schema import SelfSchemaBuilder, SystemSelfSchema
 from .trace_store import TraceFixture, TraceRecord, TraceRecorder, TraceStore
 
 __all__ = [
+    "JsonlMetricsStore",
+    "MetricsSample",
     "ReplayState",
+    "RuntimeMetrics",
+    "SelfSchemaBuilder",
+    "SystemSelfSchema",
     "TraceFixture",
     "TraceRecord",
     "TraceRecorder",
     "TraceReplayer",
     "TraceStore",
+    "derive_runtime_metrics",
 ]
