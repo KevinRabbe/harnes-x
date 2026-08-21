966e0ad2f7116a0f27a8af72c721ea73aa8e94be
KevinRabbe <rabbekevin@icloud.com>
2026-08-21 04:08:00 +0200

    M21: expose coding runtime

---
+++ src/harness_x/coding/__init__.py
@@ -0,0 +1,9 @@
+"""Coding-task runtime for Harness X."""
+
+from .runtime import CodingTaskReport, CodingTaskRuntime, CodingVerificationResult
+
+__all__ = [
+    "CodingTaskReport",
+    "CodingTaskRuntime",
+    "CodingVerificationResult",
+]
