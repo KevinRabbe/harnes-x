8940cbf73990df173c6f71ddb716828974162c92
KevinRabbe <rabbekevin@icloud.com>
2026-08-21 13:25:51 +0200

    M21: export controller-owned coding runtime

--- src/harness_x/coding/__init__.py
+++ src/harness_x/coding/__init__.py
@@ -1,8 +1,10 @@
 """Coding-task runtime for Harness X."""
 
+from .autonomous_runtime import AutonomousCodingTaskRuntime
 from .runtime import CodingTaskReport, CodingTaskRuntime, CodingVerificationResult
 
 __all__ = [
+    "AutonomousCodingTaskRuntime",
     "CodingTaskReport",
     "CodingTaskRuntime",
     "CodingVerificationResult",
