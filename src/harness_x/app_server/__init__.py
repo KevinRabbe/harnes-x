86309d99a987b3ba02425a7f505bce908a794c51
KevinRabbe <rabbekevin@icloud.com>
2026-08-22 05:33:10 +0200

    M34: export local app server contracts

---
+++ src/harness_x/app_server/__init__.py
@@ -0,0 +1,26 @@
+"""Local single-user Harness X App Server foundation."""
+
+from .protocol import (
+    AppEvent,
+    AppEventKind,
+    AppServerError,
+    AppServerHealth,
+    AppSessionSnapshot,
+    AppSessionStatus,
+    CodingSessionRequest,
+)
+from .service import AppServerService, HarnessCodingRunner
+from .store import AppSessionStore
+
+__all__ = [
+    "AppEvent",
+    "AppEventKind",
+    "AppServerError",
+    "AppServerHealth",
+    "AppServerService",
+    "AppSessionSnapshot",
+    "AppSessionStatus",
+    "AppSessionStore",
+    "CodingSessionRequest",
+    "HarnessCodingRunner",
+]
